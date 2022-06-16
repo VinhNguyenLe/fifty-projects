b2475c0daf6ac98f1de5abf59181718ea9787db8
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-16 23:39:08 +0700

    Mobile Device

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import useViewport from './useViewPort';
-import Home from './pages/Home';
-function App() {
-  const viewPort = useViewport();
-  const isTabletAndMobile = viewPort.width <= 960;
+import { BrowserRouter as Router } from 'react-router-dom'
+import useViewport from './useViewPort'
+import Home from './pages/Home'
+import Mobile from './pages/Mobile'
+import { Fragment } from 'react'
 
-  if (isTabletAndMobile) {
-    return <div className="isMobileTablet">The website only supports devices larger than 960px</div>;
-  } else {
-    return (
-      <div className="App">
-        <Router>
-          <Home />
-        </Router>
-      </div>
-    );
-  }
+function App() {
+    const viewPort = useViewport()
+    const isTabletAndMobile = viewPort.width <= 960
+
+    if (isTabletAndMobile) {
+        return (
+            <Fragment>
+                <Mobile />
+            </Fragment>
+        )
+    } else {
+        return (
+            <div className="App">
+                <Router>
+                    <Home />
+                </Router>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/pages/Mobile/index.jsx
@@ -0,0 +1,16 @@
+import './Mobile.scss'
+
+function Mobile() {
+    return (
+        <div className="MobileView">
+            <img src="https://watson-developer-cloud.github.io/react-components/images/react-logo.svg" alt="" />
+            <p className="slideUp">Hello, I'm Vinh</p>
+            <p className="slideUp">This website I made with ReactJS</p>
+            <p className="slideUp"> I just learned React a short time</p>
+            <p className="slideUp">The website only supports devices larger than than 960px</p>
+            <p className="slideUp">Please switch to the device with the appropriate browser. Thanks ^^</p>
+        </div>
+    )
+}
+
+export default Mobile
