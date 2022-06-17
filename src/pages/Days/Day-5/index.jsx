90fda72c2597678a90de92e82a0c3706ed0eac60
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-17 16:12:22 +0700

    Day 5

--- src/pages/Days/Day-5/index.jsx
+++ src/pages/Days/Day-5/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { TitleContext } from '../../../App'
 import { projectRoutes as project } from '../../../router'
 
+import './Day-5.scss'
+
 function Day5() {
     const titleDoc = React.useContext(TitleContext)
 
@@ -12,7 +14,32 @@ function Day5() {
         }
     }, [])
 
-    return <div>Day5</div>
+    const [percent, setPercent] = React.useState(1)
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setPercent((prev) => prev + 1)
+        }, 40)
+        if (percent > 99) {
+            clearInterval(timer)
+        }
+
+        return () => clearInterval(timer)
+    }, [percent])
+
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+        return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    }
+
+    let blur = 20 - percent / 5
+    return (
+        <div className="Day5">
+            <div className="bg" style={{ filter: `blur(${blur}px)` }}></div>
+            <div className="percent" style={{ opacity: scale(percent, 0, 100, 1, 0) }}>
+                {percent}%
+            </div>
+        </div>
+    )
 }
 
 export default Day5
