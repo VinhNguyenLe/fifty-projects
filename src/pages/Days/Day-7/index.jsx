8f79bd1aab43cd41e14890bfc270e55e22df41c3
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-19 19:36:00 +0700

    Day 7

--- src/pages/Days/Day-7/index.jsx
+++ src/pages/Days/Day-7/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import './Day-7.scss'
 
-function index() {
-    return <div>index</div>
+function Day7() {
+    return (
+        <div className="Day7">
+            <div className="split left">
+                <div className="split__content">
+                    <h1>Playstation 5</h1>
+                    <div className="btn">Buy now</div>
+                </div>
+            </div>
+            <div className="split right">
+                <div className="split__content">
+                    <h1>Xbox Series X</h1>
+                    <div className="btn">Buy now</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default index
+export default Day7
