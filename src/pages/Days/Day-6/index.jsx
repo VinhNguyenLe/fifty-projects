a499b1c1e120707af3c12247248d095dc806ec41
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-19 19:00:03 +0700

    Day 6

--- src/pages/Days/Day-6/index.jsx
+++ src/pages/Days/Day-6/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 
+import './Day-6.scss'
+
 function Day6({ docTitle, depCom }) {
     useEffect(() => {
         document.title = `${docTitle}`
@@ -8,7 +10,45 @@ function Day6({ docTitle, depCom }) {
         }
     }, [depCom])
 
-    return <div></div>
+    const boxCount = 10
+
+    const arrBox = []
+    for (let i = 0; i < boxCount; i++) {
+        arrBox.push(`Content ${i + 1}`)
+    }
+
+    const showBox = [0, 1, 2]
+
+    useEffect(() => {
+        const boxes = document.querySelectorAll('.box')
+
+        const handleScroll = () => {
+            boxes.forEach((box, index) => {
+                if (index > 2) {
+                    if (box.getBoundingClientRect().top < window.innerHeight - box.offsetHeight) {
+                        box.classList.add('show')
+                    } else {
+                        box.classList.remove('show')
+                    }
+                }
+            })
+        }
+
+        window.addEventListener('scroll', handleScroll)
+    }, [])
+
+    return (
+        <div className="Day6">
+            <h1>Scroll to see the animation</h1>
+            <div className="boxes">
+                {arrBox.map((item, index) => (
+                    <div key={index} className={`box ${showBox.includes(index) ? 'show' : ''}`}>
+                        {item}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Day6
