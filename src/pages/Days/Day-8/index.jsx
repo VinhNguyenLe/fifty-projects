470b1f9556e4f07cb5790370fdcdeb9a5b255fb7
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-19 21:58:30 +0700

    Day 8

--- src/pages/Days/Day-8/index.jsx
+++ src/pages/Days/Day-8/index.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import { useEffect } from 'react'
 
-function Day8() {
-    return <div>Day8</div>
+import './Day-8.scss'
+
+function Day8({ docTitle, depCom }) {
+    useEffect(() => {
+        document.title = `${docTitle}`
+        return () => {
+            document.title = `50 Projects In 50 Days Challenge`
+        }
+    }, [depCom])
+
+    const inputLabel = {
+        email: 'Email',
+        password: 'Password',
+    }
+
+    const emailLetters = inputLabel.email.split('')
+    const passwordLetters = inputLabel.password.split('')
+
+    return (
+        <div className="Day8">
+            <div className="form">
+                <form>
+                    <h1>Please Login</h1>
+                    <div className="form-control">
+                        <input required type="text" />
+                        <label>
+                            {emailLetters.map((item, index) => (
+                                <span
+                                    key={index}
+                                    style={{
+                                        transitionDelay: `${index * 50}ms`,
+                                    }}
+                                >
+                                    {item}
+                                </span>
+                            ))}
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <input required type="password" />
+                        <label>
+                            {passwordLetters.map((item, index) => (
+                                <span
+                                    key={index}
+                                    style={{
+                                        transitionDelay: `${index * 50}ms`,
+                                    }}
+                                >
+                                    {item}
+                                </span>
+                            ))}
+                        </label>
+                    </div>
+                    <button className="btn">Login</button>
+                    <p className="text">
+                        Don't have a account?
+                        <a href="#"> Register</a>
+                    </p>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Day8
