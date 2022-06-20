e34b2a32104b687f868903e31395579d7594d876
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-20 15:12:59 +0700

    Day 10

--- src/pages/Days/Day-10/index.jsx
+++ src/pages/Days/Day-10/index.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-function Day10() {
-    return <div>Day10</div>
+import './Day-10.scss'
+
+function Day10({ docTitle, depCom }) {
+    const apiURL = 'https://icanhazdadjoke.com'
+
+    useEffect(() => {
+        document.title = `${docTitle}`
+        return () => {
+            document.title = `50 Projects In 50 Days Challenge`
+        }
+    }, [depCom])
+
+    const [joke, setJoke] = useState('Click the button to get joke')
+    console.log('Log ~ joke', joke)
+
+    useEffect(() => {
+        const btn = document.querySelector('button')
+        const getData = () => {
+            fetch(apiURL, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data)
+                    setJoke(data.joke)
+                })
+        }
+        btn.addEventListener('click', getData)
+
+        return () => {
+            btn.removeEventListener('click', getData)
+        }
+    }, [joke])
+
+    return (
+        <div className="Day10">
+            <div className="container">
+                <h3>Don't Laugh Challenge</h3>
+                <h1>{joke}</h1>
+                <button>Get Another Joke</button>
+            </div>
+        </div>
+    )
 }
 
 export default Day10
