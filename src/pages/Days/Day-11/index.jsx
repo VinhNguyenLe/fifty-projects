4db1aed1e73fce805b9d1cc18501939e2e800f9e
VinhNguyenLe <levinhbn99@gmail.com>
2022-06-21 22:13:34 +0700

    Day 11

--- src/pages/Days/Day-11/index.jsx
+++ src/pages/Days/Day-11/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useLayoutEffect } from 'react'
 
 import './Day-11.scss'
 
@@ -9,7 +9,45 @@ function Day11({ docTitle, depCom }) {
             document.title = `50 Projects In 50 Days Challenge`
         }
     }, [depCom])
-    return <div className="Day11"></div>
+
+    const [state, setState] = useState(false)
+    const [key, setKey] = useState([])
+
+    useEffect(() => {
+        const getKey = (e) => {
+            setState(true)
+            setKey([e.key, e.keyCode, e.code])
+        }
+        window.addEventListener('keydown', getKey)
+        return () => {
+            window.removeEventListener('keydown', getKey)
+        }
+    }, [window, key])
+
+    return (
+        <div className="Day11">
+            <div className="wrapper">
+                {state ? (
+                    <div className="inner">
+                        <div className="key">
+                            <p>event.key</p>
+                            <h3>{key[0]}</h3>
+                        </div>
+                        <div className="key">
+                            <p>event.keyCode</p>
+                            <h3>{key[1]}</h3>
+                        </div>
+                        <div className="key">
+                            <p>event.code</p>
+                            <h3>{key[2]}</h3>
+                        </div>
+                    </div>
+                ) : (
+                    <h2 className="key">Press any key to get the keyCode</h2>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Day11
